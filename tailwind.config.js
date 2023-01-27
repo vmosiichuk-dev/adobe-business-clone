74a01ad5f8b25477460c763e62713d4f62ac098e
vladnomad <vlad.nmd@gmail.com>
2023-01-27 19:04:35 +0100

    Side to side comparison for Mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,36 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  mode: 'jiit',
+  mode: 'jit',
   content: [
     './public/**/*.html',
     './src/**/*.{js,jsx,ts,tsx,vue}',
   ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      stone: colors.stone,
+      sky: colors.sky,
+      blue: colors.blue,
+      black: colors.black,
+      white: colors.white,
+      neutral: colors.neutral,
+      adobe: "#fbfbfb",
+      adobered: {
+        light: "#f00f00",
+        dark: "#b40e04",
+      },
+    },
+    fontFamily: {
+      'mono': ['"Space Mono"', 'monospace'],
+      'noto': ['"Noto Sans KR"', 'sans-serif'],
+    },
+    extend: {
+      backgroundImage: {
+        'body': "url('../public/img/grid-bg1.jpg')",
+       },
+    },
   },
   plugins: [],
 }
