a1601ec87ccdc7f14151937638fd438c137be26c
vladnomad <vlad.nmd@gmail.com>
2023-03-12 23:35:24 +0100

    Dynamic HTML content on load and change (matchMedia)

---
+++ public/comp.js
@@ -0,0 +1,31 @@
+const clone = document.getElementById('clone'),
+      mql = window.matchMedia("(min-width: 600px) and (max-width: 900px)"),
+      mqlXl = window.matchMedia("(min-width: 1710px)");
+
+function load_md(){
+    fetch('md.html')
+    .then(response => response.text())
+    .then(md => document.getElementById('clone').innerHTML = md);
+}
+
+function load_sm(){
+    fetch('sm.html')
+    .then(response => response.text())
+    .then(sm => document.getElementById('clone').innerHTML = sm);
+}
+
+function mediaContentChange(event) {
+    if (event.matches) {
+        load_md();
+    } else {
+        load_sm();
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(e) { 
+    mediaContentChange(mql); 
+    mediaContentChange(mqlXl); 
+});
+
+mql.addEventListener("change", mediaContentChange);
+mqlXl.addEventListener("change", mediaContentChange);
